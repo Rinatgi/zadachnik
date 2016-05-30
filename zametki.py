bace5483ead9df1cdfb452959b92f96670397473
Rinatgi <renich_87@mail.ru>
2016-05-30 06:35:54 +0300

    для создания применили модуль jsоn При обратном преобразования строки в словарь, значения словаря входят ввиде списка. Почему?

--- zametki.py
+++ zametki.py
@@ -2,7 +2,7 @@
 #Задание
 #программа для ведения заметок.
 #1.добавить заметку,удалить, посмотреть список всех задач, список выполненных,список будущих задач.
-import shelve,sys
+import json,sys
 from datetime import datetime, date
 new_tasks = {}
 def main(): 
@@ -52,19 +52,19 @@ def new_note():
 	#print (new_task)
 	status = 'not done'
 	# создаем файл с нашими данными для задачи
-	f = shelve.open('out.txt')# для этого используем модуль shelve
-	f[new_target] = [dt_start,dt_end,status]# создаем кортеж  со словарем
-	print (f.items)
-	f.close
-
+	new_task = {'{0}:{1},{2},{3}'}.format('new_target','dt_start','dt_end','status') # создаем наш словарь с задачей.
+	f = open('Data.txt','w')#создаем файл с нашими данными
+	data = json.dump(new_task,f.wite, sort_keys = True)# преобразуем наш словарь в строку для записи в файл.
+	f.close()
 	main()# Возващаемся в начало программы
 def all_note():
-	f = shelve.open('out.txt')
-	print (f.keys)
-	f.close
+	f = open('Data.txt', 'r')
+	data = json.load(f.read)#преобразуем строку в файле обратно наш словарь
+	print (data)# смотрим список наших задач.
+	f.close()
+	main()
 	#print(u'Посмотреть список всех задач')
 def perfom_note():
-	f = shelve.open('out.txt')
 	try:
 		f.values(u'done')
 	except ValueError:
@@ -75,14 +75,12 @@ def future_note():
 	print(u'Будущие задачи')		
 def del_note():
 	del_target = raw_input(u'Введите задачу,которую хотите удалить:\n'.encode('utf-8'))
-	f = shelve.open('out.txt')
 	#проверка на правильный ввод задачи и её существовании.
 	try:
 		del f[del_target]
 	except KeyError:
 		print (u'Нет такой задачи.Повторите ввод!'.encode('utf-8'))
 		del_note()
-	f.close
 	main()
 	#print(u'Удалить задачу')
 def exit_note():
