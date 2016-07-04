3d056fc0c64170e2da606a9cb4f3ded06d37e080
Rinatgi <renich_87@mail.ru>
2016-07-04 11:31:37 +0300

    Устранены ошибки отступов

--- zametki.py
+++ zametki.py
@@ -47,9 +47,9 @@ def main():
                     4.Список будущих задач. \n
                     5.Удалить заметку. \n
                     6.Изменить задачу.'''
-    # просим ввести нужную задачу.
+        # просим ввести нужную задачу.  
         variant = raw_input(u'Выбирите значение '.encode('utf-8'))  
-    # проверка введенного значения.
+        # проверка введенного значения.  
         if variant == '1': 
             new_note()
                 
@@ -80,12 +80,12 @@ def new_note():
     """ 
     # вводим нашу задачу
     new_target = raw_input(u'Введите новую задачу: '.encode('utf-8'))
-    # Цикл проверки на правильность ввода даты 
+    # цикл проверки на правильность ввода даты 
     while True:
         # получаем у пользователя дату начала и конца задачи ввиде строки
         data_start = raw_input(
-                                u'Введите дату начала выполнения задачи '
-                                u'(год.месяц.число):\n '.encode('utf-8'))
+            u'Введите дату начала выполнения задачи '
+            u'(год.месяц.число):\n '.encode('utf-8'))
         try:
             # с помощью функции datetime преобразуем полученные строки 
             # в необходимый нам формат даты
@@ -101,8 +101,8 @@ def new_note():
 
     while True:
         data_end = raw_input(
-                            u'Введите дату завершения задачи '
-                            u'(год.месяц.число):\n'.encode('utf-8'))
+            u'Введите дату завершения задачи '
+            u'(год.месяц.число):\n'.encode('utf-8'))
         try:
             # с помощью функции datetime преобразуем полученные строки 
             # в необходимый нам формат даты
@@ -122,11 +122,10 @@ def new_note():
         'end': dt_end, 
         'status': 'not done',
         } 
-        # добавляем наш словарь в список
+    # добавляем наш словарь в список
     tasks.append(new_task)
-        # записываем наш список в файл     
-    write_file()    
-        # возвращаемся в начало программы        
+    # записываем наш список в файл     
+    write_file()          
 
 def all_note():
     """
@@ -136,9 +135,9 @@ def all_note():
         # переменная будет хранить название задачи
         target = task['target']
         # если у название задачи больше 10 символов  мы его обрезаем       
-        print (
-              u'Задача №{0}: {1:10}; {start}; {end}; {status}.'
-              .format(number, get_short_string(target), **task))   
+        print( 
+            u'Задача №{0}: {1:10}; {start}; {end}; {status}.'
+            .format(number, get_short_string(target), **task))
 
 def perfom_note():
     """    
@@ -150,9 +149,9 @@ def perfom_note():
         target = task['target']
         # если есть похожее значение в ключе мы его принтуем
         if task['status'] == 'done':
-            print (
-                  u'Задача: {0:10}; {start}; {end}; {status}.'
-                  .format(get_short_string(target), **task))
+            print(
+                u'Задача: {0:10}; {start}; {end}; {status}.'
+                .format(get_short_string(target), **task))
                         
 def future_note():
     """
@@ -166,8 +165,8 @@ def future_note():
         # сравниваем дату конца задачи с сегодняшним числом
         if task['end'] > now:
             print( 
-                  u'Задача: {0:10}; {start}; {end}; {status}'
-                  .format(get_short_string(target), **task))
+                u'Задача: {0:10}; {start}; {end}; {status}'
+                .format(get_short_string(target), **task))
 
 def del_note():
     """
@@ -178,8 +177,9 @@ def del_note():
     # вызываем функцию просмотра всех задач. 
     all_note()
     # просим пользователя ввести номер задачи  
-    del_target = input(u'Введите номер задачи,которую хотите удалить:\n'
-                      .encode('utf-8'))
+    del_target = input(
+        u'Введите номер задачи,'
+        u'которую хотите удалить:\n'.encode('utf-8'))
     # декременитируем введенное число,  
     # т.к.элементы списка начинают отчет с нуля.  
     del_task = del_target - 1
@@ -204,10 +204,8 @@ def write_file():
     # создаем новый список для записи в файл  
     write_tasks = []
     for task in tasks:
-        """
-        создадим копию нашего словаря,чтобы не вносить изменения 
-        в основном словаре
-        """
+        #создадим копию нашего словаря,
+        #чтобы не вносить изменения в основном словаре.  
         copy_task = task.copy()
         # преобразуем формат datetime  в строку для корректной записи в файл.
         copy_task['start'] = task['start'].strftime(date_format)
